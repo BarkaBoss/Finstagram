348cf9a3890b49251992ec086dc323e5993c3326
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-29 23:41:24 +0100

    Finished Login form validation

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -7,11 +9,14 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   double? _deviceHeight, _deviceWidth;
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  String? _email, _password;
 
   @override
   Widget build(BuildContext context) {
     _deviceWidth = MediaQuery.of(context).size.width;
     _deviceHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
           child: Container(
@@ -21,10 +26,7 @@ class _LoginState extends State<Login> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _titleWidget(),
-              _loginButton()
-            ],
+            children: [_titleWidget(), _loginForm(), _loginButton()],
           ),
         ),
       )),
@@ -39,12 +41,65 @@ class _LoginState extends State<Login> {
     );
   }
 
-  Widget _loginButton(){
-    return MaterialButton(onPressed: (){},
+  Widget _loginButton() {
+    return MaterialButton(
+      onPressed: () {},
       minWidth: _deviceWidth! * 0.70,
       height: _deviceHeight! * 0.06,
       color: Colors.red,
-      child: const Text("Login", style: TextStyle(color: Colors.white),),
+      child: const Text(
+        "Login",
+        style: TextStyle(color: Colors.white),
+      ),
     );
-}
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _deviceHeight! * 0.2,
+      child: Form(
+          key: _loginFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _emailTextField(),
+              _passwordTextField()
+            ],
+          )),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email Address..."),
+      onSaved: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+      validator: (value) {
+        bool result = value!.contains(RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"));
+
+        result ? null : "Please Enter a Valid Email";
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+        decoration: const InputDecoration(hintText: "Password"),
+        obscureText: true,
+        obscuringCharacter: "*",
+        onSaved: (value) {
+          setState(() {
+            _password = value;
+          });
+        },
+        validator: (value) => value!.length > 6
+            ? null
+            : "Password should be 6 characters minimum");
+  }
 }
