34ed5adefd934c4ef97859fa2c1bf2ebf1b5c6a5
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-29 23:16:07 +0100

    Added Login UI Components

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget{
-
+class Login extends StatefulWidget {
   @override
   State createState() => _LoginState();
 }
 
-class _LoginState extends State<Login>{
+class _LoginState extends State<Login> {
+  double? _deviceHeight, _deviceWidth;
 
   @override
   Widget build(BuildContext context) {
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(color: Colors.blue,),
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _titleWidget(),
+              _loginButton()
+            ],
+          ),
+        ),
+      )),
     );
   }
+
+  Widget _titleWidget() {
+    return const Text(
+      "Finstagram",
+      style: TextStyle(
+          color: Colors.black45, fontWeight: FontWeight.w600, fontSize: 25),
+    );
+  }
+
+  Widget _loginButton(){
+    return MaterialButton(onPressed: (){},
+      minWidth: _deviceWidth! * 0.70,
+      height: _deviceHeight! * 0.06,
+      color: Colors.red,
+      child: const Text("Login", style: TextStyle(color: Colors.white),),
+    );
+}
 }
