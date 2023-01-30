5be5d60aaeff6164aa01b96b3df2140b835641ff
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-30 10:53:47 +0100

    Added Register validation

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -7,7 +9,8 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   double? _deviceWidth, _deviceHeight;
-  GlobalKey<FormState> _registerFormState = GlobalKey<FormState>();
+  String? _name, _email, _password;
+  final GlobalKey<FormState> _registerFormState = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +26,7 @@ class _RegisterState extends State<Register> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [_titleWidget(), _registerButton()],
+              children: [_titleWidget(), _registerForm(), _registerButton()],
             ),
           ),
         ),
@@ -40,11 +43,81 @@ class _RegisterState extends State<Register> {
   }
 
   Widget _registerButton(){
-    return MaterialButton(onPressed: (){},
+    return MaterialButton(onPressed: (){
+      _registerUser;
+    },
       minWidth: _deviceWidth! * 0.7,
       height: _deviceHeight! * 0.06,
       color: Colors.red,
     child: const Text("Register", style: TextStyle(color: Colors.white,),),
     );
+  }
+
+  Widget _registerForm(){
+    return Container(
+      height: _deviceHeight! * 0.2,
+      child: Form(
+        key: _registerFormState,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _nameTextField(),
+              _emailTextField(),
+              _passwordTextField()
+            ],
+          )),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email Address..."),
+      onSaved: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+      validator: (value) {
+        bool result = value!.contains(RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"));
+
+        return result ? null : "Please enter a valid email";
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+        decoration: const InputDecoration(hintText: "Password"),
+        obscureText: true,
+        obscuringCharacter: "*",
+        onSaved: (value) {
+          setState(() {
+            _password = value;
+          });
+        },
+        validator: (value) => value!.length > 6
+            ? null
+            : "Password should be 6 characters minimum");
+  }
+
+  Widget _nameTextField(){
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Username"),
+      onSaved: (value){
+        setState(() {
+          _name = value;
+        });
+      },
+      validator: (value)=> value!.isEmpty ? null : "Name can not be blank",
+    );
+  }
+
+  void _registerUser(){
+    if(_registerFormState.currentState!.validate()){
+      _registerFormState.currentState!.save();
+    }
   }
 }
