35c31149d037d4355a0b281dc8b8e9ed62f7c181
BarkaBoss <bossmajor.biii@gmail.com>
2023-02-01 15:52:44 +0100

    Finished Bottom Navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,21 @@
-
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
-
+class HomePage extends StatefulWidget {
   @override
   State createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
 
+  int _currentPageIndex = 0;
+  final List<Widget> _demoPages = [
+    Container(
+      color: Colors.indigo,
+    ),
+    Container(
+      color: Colors.amber,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +23,35 @@ class _HomePageState extends State<HomePage>{
         title: const Text("Finstagram"),
         actions: [
           GestureDetector(
-            onTap: (){},
+            onTap: () {},
             child: const Icon(Icons.add_a_photo),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 10.0, right: 10.0),
             child: GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: const Icon(Icons.logout),
             ),
           ),
         ],
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: _demoPages[_currentPageIndex],
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(items: const [
+      BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
+      BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Profile')
+    ],
+    currentIndex: _currentPageIndex,
+    onTap: (index){
+      setState(() {
+        _currentPageIndex = index;
+      });
+      debugPrint(index.toString());
+    },
     );
   }
 }
